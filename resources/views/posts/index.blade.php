67775f1330735c936d8c0391dc6357099da49e9d
Irfan Daniel <muhdirfandaniel1@gmail.com>
2024-01-23 15:45:11 +0800

    change something in the views

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,13 +8,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-    <title>CRUDPosts</title>
+    <title>This is a Training Website</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-warning">
         <div class="container-fluid">
-            <a class="navbar-brand h1" href={{ route('posts.index') }}>CRUDPosts</a>
+            <a class="navbar-brand h1" href={{ route('posts.index') }}>This is a Training Website</a>
             <div class="justify-end ">
                 <div class="col ">
                     <a class="btn btn-sm btn-success" href={{ route('posts.create') }}>Add Post</a>
